76b3b155e5eca12a9fefd8b4facabbf7dce2666b
RDX-Rudra <dasrudra234@gmail.com>
2023-12-26 23:50:12 +0530

    night

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,23 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
 const Feed = () => {
   return (
-    <Stack>
-      
+    <Stack sx={{ flexDirection: { sx: "column", md: "row"}}}>
+      <Box sx={{height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
+        <Sidebar />
+
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
+          Copyright 2023 Rdx Rudra
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography>
+          <span></span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
