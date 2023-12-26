e3251b6216933d1632716cc7a6333dbe4d567b88
RDX-Rudra <dasrudra234@gmail.com>
2023-12-26 19:30:58 +0530

    evening

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './component
 
 const App = () => (
   <BrowserRouter>
-    <Box sx = {{background: '#000'}}>
+    <Box sx = {{backgroundColor: '#000'}}>
       <Navbar />
       <Routes>
         <Route path='/' exact element={<Feed/>}/>
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>
+    <Stack>
       
-    </div>
+    </Stack>
   )
 }
 
